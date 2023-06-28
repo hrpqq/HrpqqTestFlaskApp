79a3ea8c0c872a89ed10e78c8ce2e6f3ea2432a3
Ran Hao (Chinasoft Shanghai) <v-ranhao@microsoft.com>
2023-06-28 17:23:21 +0800

    add checkpoint storage

--- consumer.py
+++ consumer.py
@@ -1,22 +1,30 @@
-import asyncio
 from azure.eventhub import EventHubConsumerClient
 from azure.eventhub.extensions.checkpointstoreblobaio import (
     BlobCheckpointStore,
 )
+from azure.identity import DefaultAzureCredential
 
-# BLOB_STORAGE_CONNECTION_STRING = ""
-# BLOB_CONTAINER_NAME = "BLOB_CONTAINER_NAME"
+BLOB_STORAGE_ACCOUNT_URL = "hrpqqtestsa.core.windows.net"
+# BLOB_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=hrpqqtestsa;EndpointSuffix=core.windows.net"
+BLOB_CONTAINER_NAME = "checkpoints"
 EVENT_HUB_CONNECTION_STR = "Endpoint=sb://hrpqqtesteventhubns.servicebus.windows.net/;SharedAccessKeyName=first;SharedAccessKey=EKx/IHO/UFXjc785qV4B2owxJFvOBBXga+AEhGerrIU=;EntityPath=hub1"
 EVENT_HUB_NAME = "hub1"
 
 class Consumer:
     def __init__(self) -> None:
+        self.credential = DefaultAzureCredential()
+        # Create an Azure blob checkpoint store to store the checkpoints.
+        checkpoint_store = BlobCheckpointStore.from_connection_string(
+            blob_account_url=BLOB_STORAGE_ACCOUNT_URL, 
+            container_name=BLOB_CONTAINER_NAME,
+            credential=self.credential,
+        )
         # Create a consumer client for the event hub.
         self.client = EventHubConsumerClient.from_connection_string(
             conn_str=EVENT_HUB_CONNECTION_STR,
             consumer_group="$Default",
             eventhub_name=EVENT_HUB_NAME,
-            # checkpoint_store=checkpoint_store,
+            checkpoint_store=checkpoint_store,
         )
         self.names = []
 
@@ -32,15 +40,12 @@ class Consumer:
 
         # Update the checkpoint so that the program doesn't read the events
         # that it has already read when you run it next time.
-        # await partition_context.update_checkpoint(event)
+        partition_context.update_checkpoint(event)
 
     def listen(self):
         with self.client:
             self.client.receive(on_event=self.on_event, starting_position="-1")
-        # Create an Azure blob checkpoint store to store the checkpoints.
-        #checkpoint_store = BlobCheckpointStore.from_connection_string(
-        #    BLOB_STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME
-        #)
+        self.credential.close()
 
         # Call the receive method. Read from the beginning of the
         # partition (starting_position: "-1")
